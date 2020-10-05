c0bc70f1a62b3a27b35d7e0e126d15f226536381
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-10-05 21:19:17 +0100

    LESSON 8 - code

---
+++ DropBall/DropBall/PlatformRemover.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Stride.Engine;
+using Stride.Physics;
+
+namespace DropBall
+{
+    public class PlatformRemover: AsyncScript
+    {    
+        public override async Task Execute()
+        {
+            var physics = Entity.Get<RigidbodyComponent>();
+            while(Game.IsRunning)
+            {
+                var collision = await physics.NewCollision();
+                
+                var entity = collision.ColliderA == physics
+                    ? collision.ColliderB.Entity
+                    : collision.ColliderA.Entity;
+                
+                Entity.Scene.Entities.Remove(entity);
+            }
+        }
+    }
+}
