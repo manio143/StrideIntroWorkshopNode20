b04396db31c006e68bb2406438163368ad47a654
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-10-05 19:18:50 +0100

    LESSON 4 - code

---
+++ DropBall/DropBall/ConstantVelocity.cs
@@ -0,0 +1,16 @@
+﻿using Stride.Core.Mathematics;
+using Stride.Engine;
+using Stride.Physics;
+
+namespace DropBall
+{
+    public class ConstantVelocity : SyncScript
+    {
+        public float Velocity { get; set; }
+
+        public override void Update()
+        {
+            Entity.Get<RigidbodyComponent>().LinearVelocity = new Vector3(0, Velocity, 0);
+        }
+    }
+}
