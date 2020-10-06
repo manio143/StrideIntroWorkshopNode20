12753469f5443ec1daaa1fa41486887bb1f0312d
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-10-06 19:42:35 +0100

    LESSON 9 - code

---
+++ DropBall/DropBall/SphereKiller.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Stride.Engine;
+using Stride.Engine.Events;
+using Stride.Physics;
+
+namespace DropBall
+{
+    public class SphereKiller: AsyncScript
+    {
+        public static EventKey GameOver = new EventKey();
+    
+        public override async Task Execute()
+        {
+            var physics = Entity.Get<RigidbodyComponent>();
+            var collision = await physics.NewCollision();
+            
+            var sphere = collision.ColliderA == physics
+                ? collision.ColliderB.Entity
+                : collision.ColliderA.Entity;
+            
+            Entity.Scene.Entities.Remove(sphere);
+            foreach(var platform in Entity.Scene.Entities.Where(e => e.Name == "Platform").ToList())
+                Entity.Scene.Entities.Remove(platform);
+                
+            GameOver.Broadcast();
+        }
+    }
+}
