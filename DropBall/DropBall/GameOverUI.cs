0edf5505c9c555f16e2e6831ff21a4375b39ff11
Marian Dziubiak <marian.dziubiak@gmail.com>
2020-10-06 20:09:08 +0100

    LESSON 10 - code

---
+++ DropBall/DropBall/GameOverUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Stride.UI;
+using Stride.Engine;
+using Stride.Engine.Events;
+
+namespace DropBall
+{
+    public class GameOverUI : AsyncScript
+    {
+        public UIPage UIPage;
+
+        public override async Task Execute()
+        {
+            var receiver = new EventReceiver(SphereKiller.GameOver);
+            await receiver.ReceiveAsync();
+            
+            var ui = Entity.GetOrCreate<UIComponent>();
+            ui.Page = UIPage;
+        }
+    }
+}
